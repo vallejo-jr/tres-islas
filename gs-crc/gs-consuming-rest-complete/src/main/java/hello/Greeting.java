afe4142fdaa32d408ed34280d33b5e2110fab786
vallejo-jr <vallejo.jr@gmail.com>
2014-05-08 11:51:57 +0200

    OPEN - task 1: Meter javadoc http://github.com/vallejo-jr/tres-islas/issues/issue/1

--- gs-crc/gs-consuming-rest-complete/src/main/java/hello/Greeting.java
+++ gs-crc/gs-consuming-rest-complete/src/main/java/hello/Greeting.java
@@ -10,15 +10,28 @@ public class Greeting {
     private  String content;
 
     
+    /**
+     * @param id
+     */
     public void setId (long id) {
       this.id = id;
     }
     
+    /**
+     * @param content
+     */
     public void setContent (String content) {
       this.content = content;
     }
+    /**
+     * 
+     */
     public Greeting() {
   }
+    /**
+     * @param id
+     * @param content
+     */
     public Greeting(long id, String content) {
       this.id = id;
       this.content = content;
